c989f9ca23a8bdf71ee51988bcbfdc9b90dae6ab
Maks <mn27305@gmail.com>
2019-10-16 18:03:04 +0200

    3 exercise

--- script2.js
+++ script2.js
@@ -4,88 +4,65 @@ let money, time;
 
 
 
-// let money = +prompt(" Your monthly budget?");
-// let time = prompt(" Please enter date in format YYYY-MM-DD");
 function start () {
  money = +prompt(" Your monthly budget?");
  time = prompt(" Please enter date in format YYYY-MM-DD");
-} 
-money = +prompt(" Your monthly budget?");
- time = prompt(" Please enter date in format YYYY-MM-DD");
 
-let appData = {
-budget: money,
-timeData: time,
-expenses: {},
-optionalExpenses: {},
-income: [],
-savings: false,
+ while(isNaN(money) || money == "" || money == null) {
 
-};
-
-// let OR = prompt("Enter a required cost item this month");
-// let OR1 = prompt("Enter a required cost item this month");
-// let VO = prompt("How much will it costs?");
-// let VO1 = prompt("How much will it costs?");
-// expences =()
-
-// appData.expenses.OR = OR1;
-// appData.expenses.VO = VO1;
-
-// alert(appData.budget / 30);
-// console.log(appData.expenses);
-
-
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Enter a required cost item this month", ""),
-        b = prompt("How much will it costs?", "");
-
-    if ( (typeof(a))=== "string" && (typeof(a)) != null && (typeof(b)) != null 
-    && a != " " && b != " " && a.length < 50) {
-          console.log("done");
-          appData.expenses[a] = b;  
-      } else {
-          break;
-    }
-};
-//////////////
-// let a = prompt("Enter a required cost item this month", ""),
-//         b = prompt("How much will it costs?", ""),
-//         i =0;
-// while ( (typeof(a))=== "string" && (typeof(a)) != null && (typeof(b)) != null 
-// && a != "" && b != "" && a.length < 50 && i < 2) {
-//     console.log("Done");
-//           appData.expenses[a] = b;
-//           i++;
-          
-
-// }
-
-
-//////////////
-// let i = 0;
-
-// do {// Delai
-//     let a = prompt("Enter a required cost item this month", ""),
-//         b = prompt("How much will it costs?", "");
-//          console.log("done");
-//           appData.expenses[a] = b;
-//           i++;
-// }
-
+    money = +prompt(" Your monthly budget?");
+ }
+}
+start();
     
-// while ( (typeof(a))=== "string" && (typeof(a)) != null && (typeof(b)) != null 
-// && a != "" && b != "" && a.length < 50 && i < 2);// do teh por poka vipolnajetsa while
-////////////
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+};
 
 
 
+function chooseExpenses () {//kak tsatj koda sohranjaem funkstsiju
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Enter a required cost item this month", ""),
+            b = prompt("How much will it costs?", "");
+    
+        if ( typeof(a)=== "string" && typeof(a) != null && typeof(b) != null 
+        && a != "" && b != "" && a.length < 50) {
+              console.log("done");
+              appData.expenses[a] = b;  
+          } else {
+              i = i - 1;
+        }
+    };
+}
+chooseExpenses();// Vizivaem
+
+// money = +prompt(" Your monthly budget?");
+//  time = prompt(" Please enter date in format YYYY-MM-DD");
+function app () {
+// let appData = {
+// budget: money,
+// timeData: time,
+// expenses: {},
+// optionalExpenses: {},
+// income: [],
+// savings: false,
+// }
+};
 
 
-
-appData.moneyPerDay = appData.budget / 30;
-
+appData.moneyPerDay = (appData.budget / 30).toFixed();//POsle metoda vsegda skobki
+                                          //To fixed pustije skobki budet tseloe, 1 budet odin znak pose zapjatoi
+//Tofixed sdelat vsegda string
 alert("Day budget is: " + appData.moneyPerDay);
+// console.log(appData.budget);
+// console.log(appData.moneyPerDay);
+
 
 if(appData.moneyPerDay < 100) {
     console.log("This is minimum budget")
@@ -97,12 +74,3 @@ if(appData.moneyPerDay < 100) {
     console.log("Something goes wrong! ")
 }
 
-
-
-///////////////////
-//Funktion
-function showFirstMessage(text) {
-    alert(text);
-}
-
-showFirstMessage("Hello World ! ");
